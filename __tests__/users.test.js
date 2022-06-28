995dec2f9354187a932fabce27c6ba9aa3514c9d
Will Gundy <willgunderson503@gmail.com>
2022-06-28 16:19:22 -0700

    wrote test for creating a user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,12 +2,17 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = { email: 'test@example.com', password: '123456' };
 
 const registerAndLogin = async (userProps = {}) => {
   //?? difference
   const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 }
 
 describe('backend-express-template routes', () => {
@@ -15,7 +20,12 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
   it('signs up a new user with mock data', () => {
-    
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'test@example.com'
+    })
   });
 
   it('signs in an existing user', () => {
